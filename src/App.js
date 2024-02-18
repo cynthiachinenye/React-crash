9482f3b10771e6754136ec507c9a19e409c3cb99
cynthiachinenye <cynthiachiagu@gmail.com>
2024-02-18 21:39:36 +0100

    crash-course

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
- import './App.css';
- import Job from './Components/Job';
+import './App.css';
+import Job from './Components/Job';
 import Planet from './Components/Planet';
 function App() {
   return (
     <div className="App">
-    <Job  salary={90000} position='Senior SDE' company='Amazon'/>
-    <Job  salary={12000} position='Junior SDE' company='Google'/>
-    <Job  salary={10000} position='Project Manager' company='Netflix'/>
+      <Job salary={90000} position='Senior SDE' company='Amazon' />
+      <Job salary={12000} position='Junior SDE' company='Google' />
+      <Job salary={10000} position='Project Manager' company='Netflix' />
 
-       <Planet/>
+      <Planet />
+
+     
+    
     </div>
   );
 }
---
+++ src/Components/HookState.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+const HookState = () => {
+    const [age,setAge] = useState(0)
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HookState
