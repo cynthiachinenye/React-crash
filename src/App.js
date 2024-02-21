e8a34276d794ca74df31ac61355c5408a344bde6
cynthiachinenye <cynthiachiagu@gmail.com>
2024-02-21 12:59:45 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import './App.css';
-import Job from './Components/Job';
-import Planet from './Components/Planet';
+import HookState from './Components/HookState';
+// import Job from './Components/Job';
+// import Planet from './Components/Planet';
+
 function App() {
   return (
     <div className="App">
-      <Job salary={90000} position='Senior SDE' company='Amazon' />
-      <Job salary={12000} position='Junior SDE' company='Google' />
-      <Job salary={10000} position='Project Manager' company='Netflix' />
-
-      <Planet />
-
      
+     <HookState/>
     
-    </div>
+     </div>
+
   );
 }
 
--- src/Components/HookState.js
+++ src/Components/HookState.js
@@ -1,9 +1,25 @@
 import React, { useState } from 'react'
 
 const HookState = () => {
-    const [age,setAge] = useState(0)
+    const [count,setCount] = useState(0);
+
+    const increment =()=>{
+        setCount(count + 1);
+    }
+    const decrement =()=>{
+        setCount (count - 1)
+        
+    }
+    const zero = () =>{
+        setCount(0)
+    }
+
   return (
     <div>
+    {count}<br></br>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>derement</button>
+      <button onClick={zero}>set to Zero</button>
       
     </div>
   )
